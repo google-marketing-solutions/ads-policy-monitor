8d66364ee0bedcafc432a59d38f5fb8456c02415
Tom Symonds <tsymonds@google.com>
2023-10-13 14:55:39 +0100

    Add support for adding BQ views.
    By adding schema definitions we can auto upload views as part of the deployment.
    
    This change adds a view that shows ad groups that have no ads approved.
    
    Change-Id: Ibec6dc70182b8615863919c5d29d49809b6ecdcb

---
+++ bigquery/views/no_approved_ads_ad_group.sql
@@ -0,0 +1,53 @@
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+/**
+ * A view that shows ad groups that have no approved ads.
+ */
+WITH AdCounts AS (
+  SELECT
+    customer_id,
+    customer_descriptive_name,
+    ad_group_id,
+    ad_group_name,
+    COUNT(*) AS number_of_ads,
+  FROM
+    `${BQ_DATASET}.ad_policy_data`
+  WHERE
+    CAST(_PARTITIONTIME AS DATE) = CURRENT_DATE()
+  GROUP BY
+    1,2,3,4
+),
+DisapprovedAds AS (
+  SELECT
+    customer_id,
+    ad_group_id,
+    COUNT(*) AS disapproved_ads,
+  FROM
+    `${BQ_DATASET}.ad_policy_data`
+  WHERE
+    CAST(_PARTITIONTIME AS DATE) = CURRENT_DATE()
+    AND ad_group_ad_policy_summary_approval_status != 'APPROVED'
+  GROUP BY
+    1,2
+)
+
+SELECT
+  AdCounts.*,
+  DisapprovedAds.disapproved_ads
+FROM
+  DisapprovedAds
+LEFT JOIN
+  AdCounts USING(customer_id, ad_group_id)
+WHERE
+  DisapprovedAds.disapproved_ads = AdCounts.number_of_ads
